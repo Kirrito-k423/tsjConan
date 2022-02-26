6d7edd4eada6674e67a143d512879803bd2fd1e8
supertan <943648187@qq.com>
2022-02-26 20:58:12 +0800

    bug remains

---
+++ conanfile.py
@@ -0,0 +1,52 @@
+from conans import ConanFile, CMake, tools
+
+
+class TsjConanConan(ConanFile):
+    name = "tsjConan"
+    version = "0.0.1"
+    license = "MIT"
+    author = "shaojiemike shaojiemike@mail.ustc.edu.cn"
+    url = "https://github.com/Kirrito-k423/tsjConan"
+    description = "shaojie's C++ static/dynamic lib using conan"
+    topics = ("tsj","shaojiemike")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    exports_sources = "src/include/*", "src/*"
+#     def source(self):
+#         self.run("git clone https://github.com/conan-io/hello.git")
+#         # This small hack might be useful to guarantee proper /MT /MD linkage
+#         # in MSVC if the packaged project doesn't have variables to set it
+#         # properly
+#         tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
+#                               '''PROJECT(HelloWorld)
+# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+# conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["tsjCommonFunc"]
+
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "./include/tsjCommonFunc.h"
+
+int main (void) {
+    splitPrint("This is shaojie's Conan experience!!!");
+}
